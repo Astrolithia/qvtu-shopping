f05e33bed2adfbcd8c45ce414696979bae7f98b4
“Astrolithia” <astrolithia@icloud.com>
2025-03-24 14:42:00 +0800

    修复bug完成

--- qvtu-shopping-backend/src/main/java/com/qvtu/repository/AddressRepository.java
+++ qvtu-shopping-backend/src/main/java/com/qvtu/repository/AddressRepository.java
@@ -22,19 +22,19 @@ public interface AddressRepository extends JpaRepository<Address, Long> {
      * @param customerId 客户ID
      * @return 默认配送地址
      */
-    Optional<Address> findByCustomerIdAndIsDefaultShippingTrue(Long customerId);
+    Optional<Address> findByCustomerIdAndDefaultShippingTrue(Long customerId);
     
     /**
      * 根据客户ID查询默认账单地址
      * @param customerId 客户ID
      * @return 默认账单地址
      */
-    Optional<Address> findByCustomerIdAndIsDefaultBillingTrue(Long customerId);
+    Optional<Address> findByCustomerIdAndDefaultBillingTrue(Long customerId);
     
     /**
      * 根据客户ID查询同时是默认配送和默认账单的地址
      * @param customerId 客户ID
      * @return 同时是默认配送和默认账单的地址
      */
-    Optional<Address> findByCustomerIdAndIsDefaultShippingTrueAndIsDefaultBillingTrue(Long customerId);
+    Optional<Address> findByCustomerIdAndDefaultShippingTrueAndDefaultBillingTrue(Long customerId);
 } 
